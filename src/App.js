275032f643e222cbc8d1ee2dd6df2ebb12f7885a
shellcya <shellcya_32@hotmail.com>
2020-09-18 20:48:12 -0300

    implementacao html aquecedor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Aquecedor from './components/Aquecedor/Aquecedor';
 import Menu from './components/Menu/Menu.js';
 
 
-function App() {
+function App()  {
   return (
     <div className="App">
       <Menu />
+      <Aquecedor/>
     </div>
   );
 }
---
+++ src/components/Aquecedor/Aquecedor.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Aquecedor = () => {
+    return (
+      <div>
+      <h2>Obtenha também </h2>
+      <h4>Preencha para criar!</h4>
+
+      <input type="radio" id="option" name="opçao" value="zero"/>
+  <label  for="option0">It is long established fact that a reader</label>
+
+  <input type="radio" id="option0" name="opçao" value="one"/>
+  <label for="option">It is long established fact that a reader</label>
+  
+  <input type="radio" id="option2" name="opçao" value="two"/>
+  <label  for="option2">It is long established fact that a reader</label>
+
+  <input type="radio" id="option3" name="opçao" value="three"/>
+  <label  for="option3">It is long established fact that a reader</label>
+  
+  <h3>Você vai precisar de:</h3>
+      </div>
+       
+
+    )
+}
+ 
+ 
+   
+  
+  export default Aquecedor;
