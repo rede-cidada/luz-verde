ce6382452965c7fa08906e2c844bc3177ed4edd5
shellcya <shellcya_32@hotmail.com>
2020-09-23 20:59:03 -0300

    implementação css aquecedor

--- src/components/Aquecedor/Aquecedor.js
+++ src/components/Aquecedor/Aquecedor.js
@@ -1,25 +1,39 @@
 import React from "react";
+import '../Aquecedor/Aquecedor.css'
 
 const Aquecedor = () => {
     return (
-      <div>
-      <h2>Obtenha também </h2>
-      <h4>Preencha para criar!</h4>
-
-      <input type="radio" id="option" name="opçao" value="zero"/>
-  <label  for="option0">It is long established fact that a reader</label>
-
-  <input type="radio" id="option0" name="opçao" value="one"/>
-  <label for="option">It is long established fact that a reader</label>
+      <section className="boxAquecedor">
+     
+      <h2 className="titleAquecedor">Faça Seu Aquecedor </h2>
+      <h4 className="subTitle">Para quantas casas deseja fazer?</h4>
+       
+      <ul >
+         <li className="listAquecedor">
+            <input   className="optionAquecedor" type="radio" id="option" name="opçao" value="zero"/>
+            <label  for="option0">No máximo Duas</label>
+         </li>
+         <li  className="listAquecedor">
+            <input  className="optionAquecedor"  type="radio" id="option0" name="opçao" value="one"/>
+            <label for="option">No máximo Quatro</label>
+         </li>
   
-  <input type="radio" id="option2" name="opçao" value="two"/>
-  <label  for="option2">It is long established fact that a reader</label>
-
-  <input type="radio" id="option3" name="opçao" value="three"/>
-  <label  for="option3">It is long established fact that a reader</label>
+         <li  className="listAquecedor">
+          <input  className="optionAquecedor"  type="radio" id="option2" name="opçao" value="two"/>
+          <label   for="option2">No máximo Seis</label>
+         </li>
   
-  <h3>Você vai precisar de:</h3>
-      </div>
+         <li  className="listAquecedor">
+          <input  className="optionAquecedor"  type="radio" id="option3" name="opçao" value="three"/>
+          <label   for="option3">No máximo Oito</label>
+         </li>
+     </ul>
+  
+     
+   
+ 
+  </section>
+     
        
 
     )
