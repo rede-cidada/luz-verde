bf4fc3eaf2a8300200f2f22de1d6549f811e27ca
vanessa oliveira <misaki.vanessa.oliveira@gmail.com>
2020-09-26 21:48:36 -0300

    adição de icon e configuração de responsividade do componente Team

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,9 +1,12 @@
 import React from "react";
-import israel from '../../assets/img-time/ISR.png';
-import marilia from '../../assets/img-time/MAR.png';
-import mariah from '../../assets/img-time/MARÍ.png';
-import shellcya from '../../assets/img-time/SHE.png';
-import vanessa from '../../assets/img-time/VAN.png';
+import israel from "../../assets/img-time/ISR.png";
+import marilia from "../../assets/img-time/MARÍ.png";
+import mariah from "../../assets/img-time/MAR.png";
+import shellcya from "../../assets/img-time/SHE.png";
+import vanessa from "../../assets/img-time/VAN.png";
+
+import github from "../../assets/img-time/gitPNG.png";
+import linkedin from "../../assets/img-time/linkedinPNG.png";
 
 import "../Team/Team.css";
 
@@ -11,37 +14,123 @@ function Team() {
   return (
     <div>
       <h2 className="titulo_team">Quem Somos?</h2>
-      <div className="caixa_team">
-        <img className="img_team" src={israel} alt="integrante israel " />
-        <img className="img_team" src={marilia} alt="integrante marilia" />
-        <img className="img_team" src={mariah} alt="integrante mariah" />
-        <img className="img_team" src={shellcya} alt="integrante shellcya" />
-        <img className="img_team" src={vanessa} alt="integrante vanessa" />
-      </div>
-      <div className="nomes_team">
-        <p className="nome_team">Israel Batista</p>
-        <p className="nome_team">Mariah Guedes</p>
-        <p className="nome_team">Marília Andrade</p>
-        <p className="nome_team">Shellcya Habiakel</p>
-        <p className="nome_team">Vanessa Oliveira</p>
-      </div>
+      <div className="caixa_teams">
+        <div className="caixa_team">
+          <img className="img_team" src={israel} alt="integrante israel " />
+          <p className="nome_team">Israel Batista</p>
+          <p className="texto_team">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div className="caixa_icon">
+            <a href="https://github.com/Israelfer" target="_blank">
+              <img className="icon_team" src={github} alt="icon do gitHub " />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/israel-batista/"
+              target="_blank"
+            >
+              <img
+                className="icon_team"
+                src={linkedin}
+                alt="icon do linkedin "
+              />
+            </a>
+          </div>
+        </div>
 
-      <div className="textos_team">
-        <p className="texto_team">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </p>
-        <p className="texto_team">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </p>
-        <p className="texto_team">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </p>
-        <p className="texto_team">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </p>
-        <p className="texto_team">
-          Lorem ipsum dolor sit amet consectetur adipisicing
-        </p>
+        <div className="caixa_team">
+          <img className="img_team" src={mariah} alt="integrante mariah" />
+          <p className="nome_team">Mariah Guedes</p>
+          <p className="texto_team">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div className="caixa_icon">
+            <a href="https://github.com/mariahgll" target="_blank">
+              <img className="icon_team" src={github} alt="icon do gitHub " />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mariah-guedes-96087b1ab/"
+              target="_blank"
+            >
+              <img
+                className="icon_team"
+                src={linkedin}
+                alt="icon do linkedin "
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="caixa_team">
+          <img className="img_team" src={marilia} alt="integrante marilia" />
+          <p className="nome_team">Marília Andrade</p>
+          <p className="texto_team">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div className="caixa_icon">
+            <a href="https://github.com/mariliandrad" target="_blank">
+              <img className="icon_team" src={github} alt="icon do gitHub " />
+            </a>
+            <a href="https://www.linkedin.com/in/mariliandrad/" target="_blank">
+              <img
+                className="icon_team"
+                src={linkedin}
+                alt="icon do linkedin "
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="caixa_team">
+          <img className="img_team" src={shellcya} alt="integrante shellcya" />
+          <p className="nome_team">Shellcya Habiakel</p>
+          <p className="texto_team">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div className="caixa_icon">
+            <a href="https://github.com/Shellcya" target="_blank">
+              <img className="icon_team" src={github} alt="icon do gitHub " />
+            </a>
+            <a
+              href="http://linkedin.com/in/shellcya-habiakel-1344301ab"
+              target="_blank"
+            >
+              <img
+                className="icon_team"
+                src={linkedin}
+                alt="icon do linkedin "
+              />
+            </a>
+          </div>
+        </div>
+
+        <div className="caixa_team">
+          <img className="img_team" src={vanessa} alt="integrante vanessa" />
+          <p className="nome_team">Vanessa Oliveira</p>
+          <p className="texto_team">
+            Falar sobre mim e complicado mas fazendo um resumo eu gosto de
+            gatos, cachorros, fotos, cafeterias e cheiro de café, queria um dia
+            ser barista e algumas outras profissões listadas no meu gitHub,
+            escrevo uns textos e tenho uma lojinha chamada suncake.nessa
+            gostaria muito de cativar o proximo com minha arte, obrigada por ver
+            o site e lembre-se bebam água.
+          </p>
+          <div className="caixa_icon">
+            <a href="https://github.com/nessalive" target="_blank">
+              <img className="icon_team" src={github} alt="icon do gitHub " />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/vanessa-oliveira-0036a4154/"
+              target="_blank"
+            >
+              <img
+                className="icon_team"
+                src={linkedin}
+                alt="icon do linkedin "
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
