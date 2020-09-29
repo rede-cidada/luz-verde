31e24d435130c3f4039542f0f10334eca1d43bed
shellcya <shellcya_32@hotmail.com>
2020-09-28 22:06:55 -0300

    responsividade aquecedor

--- src/components/Aquecedor/Aquecedor.js
+++ src/components/Aquecedor/Aquecedor.js
@@ -2,44 +2,45 @@ import React from "react";
 import '../Aquecedor/Aquecedor.css'
 
 const Aquecedor = () => {
-    return (
-      <section className="boxAquecedor">
-     
-      <h2 className="titleAquecedor">Faça Seu Aquecedor </h2>
-      <h4 className="subTitle">Para quantas casas deseja fazer?</h4>
-       
-      <ul >
-         <li className="listAquecedor">
-            <input   className="optionAquecedor" type="radio" id="option" name="opçao" value="zero"/>
-            <label  for="option0">No máximo Duas</label>
-         </li>
-         <li  className="listAquecedor">
-            <input  className="optionAquecedor"  type="radio" id="option0" name="opçao" value="one"/>
-            <label for="option">No máximo Quatro</label>
-         </li>
-  
-         <li  className="listAquecedor">
-          <input  className="optionAquecedor"  type="radio" id="option2" name="opçao" value="two"/>
-          <label   for="option2">No máximo Seis</label>
-         </li>
-  
-         <li  className="listAquecedor">
-          <input  className="optionAquecedor"  type="radio" id="option3" name="opçao" value="three"/>
-          <label   for="option3">No máximo Oito</label>
-         </li>
-     </ul>
-  
-     
-   
- 
-  </section>
-     
-       
+   return (
 
-    )
+      <section className="boxAquecedor">
+
+         <h2 className="titleAquecedor">Faça Seu Aquecedor </h2>
+         <h4 className="subTitle">Para quantas casas deseja fazer?</h4>
+
+         <ul >
+            <li className="listAquecedor">
+               <input className="optionAquecedor" type="radio" id="option" name="opçao" value="zero" />
+               <label for="option0">No máximo Duas</label>
+            </li>
+            <li className="listAquecedor">
+               <input className="optionAquecedor" type="radio" id="option0" name="opçao" value="one" />
+               <label for="option">No máximo Quatro</label>
+            </li>
+
+            <li className="listAquecedor">
+               <input className="optionAquecedor" type="radio" id="option2" name="opçao" value="two" />
+               <label for="option2">No máximo Seis</label>
+            </li>
+
+            <li className="listAquecedor">
+               <input className="optionAquecedor" type="radio" id="option3" name="opçao" value="three" />
+               <label for="option3">No máximo Oito</label>
+            </li>
+         </ul>
+
+
+
+
+      </section>
+
+
+
+   )
 }
- 
- 
-   
-  
-  export default Aquecedor;
+
+
+
+
+export default Aquecedor;
