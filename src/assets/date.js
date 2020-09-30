77188fc70ca0152b0e7bb6a06beecf2983a5b3cc
shellcya <shellcya_32@hotmail.com>
2020-09-30 18:05:05 -0300

    implentação do array no date.js

--- src/assets/date.js
+++ src/assets/date.js
@@ -1,1 +1,23 @@
-//const aquecedor = [];
+const aquecedor = [
+  -{
+    "id": "001",
+    "materiais": "120 garrafas pet transparentes de 2 litros, 100 caixas de leite de 1 litro, 22m de cano de PVC e 1/2 polegadas, 40 conexões T em PVC e 1/2 polegadas, 2 canos de PVC de 100mm com 70cm de comprimentos para molde dos potes das garrafas pet, 2 fitas de altofusão ou borracha de câmara de ar, 1 estilete, 2 litros de tinta fosca preta, 1 luva, 1 martelo de borracha, 2 caixas d' água n°100, 2 colas para tubos de PVC, 2 arcos de serra, 10 pregos, 2 tábuas de madeira com no mínimo 120mm de comprimento, 2 ripas pequenas com aproximadamente 15cm de comprimento, 2 fitas crepe com largura de 19mm, 4 tampões de PVC de 20mm e 1/2 polegadas, 8 conexões L (luvas) de PVC de 20mm e 1/2 polegadas"
+  },
+
+  -{
+    "id": "002",
+    "materiais": "240 garrafas pet transparentes de 2 litros, 200 caixas de leite de 1 litro, 44m de cano de PVC e 1/2 polegadas, 80 conexões T em PVC e 1/2 polegadas, 4 canos de PVC de 100mm com 70cm de comprimentos para molde dos potes das garrafas pet, 4 fitas de altofusão ou borracha de câmara de ar, 1 estilete, 4 litros de tinta fosca preta, 1 luva, 1 martelo de borracha, 4 caixas d' água n°100, 4 colas para tubos de PVC, 4 arcos de serra, 20 pregos, 4 tábuas de madeira com no mínimo 120mm de comprimento, 4 ripas pequenas com aproximadamente 15cm de comprimento, 4 fitas crepe com largura de 19mm, 8 tampões de PVC de 20mm e 1/2 polegadas, 16 conexões L (luvas) de PVC de 20mm e 1/2 polegadas"
+  },
+
+  -{
+    "id": "003",
+    "materiais": "360 garrafas pet transparentes de 2 litros, 300 caixas de leite de 1 litro, 66m de cano de PVC e 1/2 polegadas, 120 conexões T em PVC e 1/2 polegadas, 6 canos de PVC de 100mm com 70cm de comprimentos para molde dos potes das garrafas pet, 6 fitas de altofusão ou borracha de câmara de ar, 1 estilete, 6 litros de tinta fosca preta, 1 luva, 1 martelo de borracha, 6 caixas d' água n°100, 6 colas para tubos de PVC, 6 arcos de serra, 30 pregos, 6 tábuas de madeira com no mínimo 120mm de comprimento, 6 ripas pequenas com aproximadamente 15cm de comprimento, 6 fitas crepe com largura de 19mm, 12 tampões de PVC de 20mm e 1/2 polegadas, 24 conexões L (luvas) de PVC de 20mm e 1/2 polegadas"
+  },
+
+  -{
+    "id": "004",
+    "materiais": "480 garrafas pet transparentes de 2 litros, 400 caixas de leite de 1 litro, 88m de cano de PVC e 1/2 polegadas, 160 conexões T em PVC e 1/2 polegadas, 8 canos de PVC de 100mm com 70cm de comprimentos para molde dos potes das garrafas pet, 8 fitas de altofusão ou borracha de câmara de ar, 1 estilete, 8 litros de tinta fosca preta, 1 luva, 1 martelo de borracha, 8 caixas d' água n°100, 8 colas para tubos de PVC, 8 arcos de serra, 30 pregos, 8 tábuas de madeira com no mínimo 120mm de comprimento, 8 ripas pequenas com aproximadamente 15cm de comprimento, 8 fitas crepe com largura de 19mm, 16 tampões de PVC de 20mm e 1/2 polegadas, 32 conexões L (luvas) de PVC de 20mm e 1/2 polegadas"
+  }
+
+
+];
