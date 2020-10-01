0fd646b2b7b5120f15eaf23a5822a5b1582ff3ed
marilia andrade <marilia.andrade12@gmail.com>
2020-09-30 23:17:58 -0300

    Troca da imagem svg para jpg e criação da home luz verde

--- src/components/LuzVerde/LuzVerde.js
+++ src/components/LuzVerde/LuzVerde.js
@@ -1,5 +1,17 @@
 import React from "react";
+import "./LuzVerde.css";
 
 export default function LuzVerde() {
-  return <h1>Hello</h1>;
+  return (
+    <section className="imagem-home">
+      <div className="sobre-luz-verde">
+        <h1>O Luz Verde </h1>
+
+        <p>
+          É uma ideia sustentável que visa a geração de energia limpa, econômica
+          e renovável, através da utilização de garrafas pets.
+        </p>
+      </div>
+    </section>
+  );
 }
