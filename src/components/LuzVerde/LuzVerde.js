df4622ff6b82b07f4d1746061b41090320f608ea
Nessalive <misaki.vanessa.oliveira@gmail.com>
2020-10-08 18:19:41 -0300

    padronização de tamanho de subtexto

--- src/components/LuzVerde/LuzVerde.js
+++ src/components/LuzVerde/LuzVerde.js
@@ -7,7 +7,7 @@ export default function LuzVerde() {
       <div className="sobre-luz-verde">
         <h1>O Luz Verde </h1>
 
-        <p>
+        <p className="subtituloLuz">
           É uma ideia sustentável que visa a geração de energia limpa, econômica
           e renovável, através da utilização de garrafas pets.
         </p>
