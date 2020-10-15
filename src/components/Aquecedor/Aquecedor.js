ca448ce6132f3ec0378b17ab88e8dd2938db898e
Nessalive <misaki.vanessa.oliveira@gmail.com>
2020-10-15 15:20:08 -0300

    correção de frase

--- src/components/Aquecedor/Aquecedor.js
+++ src/components/Aquecedor/Aquecedor.js
@@ -33,7 +33,7 @@ const Aquecedor = () => {
         <p className="textAquecedor">{date && <p>{date[index]?.materiais}</p>}</p>
       </div>
       <div>
-        <h3 className="subTitle">instruções de como corta a caixa de leitede e como construir o aquecedor</h3>
+        <h3 className="subTitle">Instruções de como cortar a caixa de leite de e como construir o aquecedor</h3>
       <iframe className="video" width="400" height="300" src="https://www.youtube.com/embed/8TeYdtupvJM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <iframe className="video" width="400" height="300" src="https://www.youtube.com/embed/-UOAoNXo_lE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
