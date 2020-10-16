c6bd765c42c6a3d4ee3d260fbcceeda2b7982a31
Marília <marilia.andrade12@gmail.com>
2020-10-15 23:20:07 -0300

    Ajuste em aquecedor

--- src/components/Aquecedor/Aquecedor.js
+++ src/components/Aquecedor/Aquecedor.js
@@ -31,14 +31,22 @@ const Aquecedor = () => {
           </button>
         ))}
 
-        {date && <p className="textAquecedor">{date[index]?.materiais}</p>}
+        {date && <div className="textAquecedor">
+					<h3>{date[index]?.titulo}</h3>
+					{index? <p>Materiais:</p>:'Selecione o número de casas que deseja ao lado.' }
+					<p>{date[index]?.materiais}</p>
+					</div>}
       </section>
 
-      <h3 className="subTitle">Instruções de como cortar a caixa de leite de e como construir o aquecedor</h3>
+      <h3 className="subTitle">Instruções de como cortar a caixa de leite e como construir o aquecedor</h3>
 
-      <section>
-      <iframe className="video" width="400" height="300" src="https://www.youtube.com/embed/8TeYdtupvJM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <iframe className="video" width="400" height="300" src="https://www.youtube.com/embed/-UOAoNXo_lE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <section className="sectionVideos">
+      <div className="videoContainer">
+			<iframe className="video" src="https://www.youtube.com/embed/8TeYdtupvJM" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+			</div>
+			<div className="videoContainer">
+			<iframe className="video" src="https://www.youtube.com/embed/-UOAoNXo_lE" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+				</div> 
       </section>
     </div>
   );
